f93fc69d1a9bcea487bb464764c72bbc2240988c
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2019-12-30 18:57:17 +0100

    chore(app): update to be more typescript friendly

--- main.ts
+++ main.ts
@@ -1,156 +1,189 @@
 import { app, BrowserWindow, screen } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
+import { ConsoleLoggerService } from './projects/server/services/logger.service';
 
-let win: BrowserWindow = null;
-const args = process.argv.slice(1);
-const serve = args.some(val => val === '--serve');
+class Application {
 
-function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
+  /**
+   * The service who manage logs
+   */
+  private readonly _logger: ConsoleLoggerService;
+
+  /**
+   * The electron application
+   */
+  private readonly _app;
+
+  /**
+   * If the app is running in local mode or not
+   */
+  private readonly _localMode: boolean;
+
+  /**
+   * The application shell
+   */
+  private _shell: BrowserWindow;
+
+  /**
+   * Initialize a new @{Application}
+   * @param localMode If the application run in local mode or not
+   */
+  constructor(localMode: boolean) {
+    this._app = app;
+    this._shell = null;
+    this._localMode = localMode;
+    this._logger = new ConsoleLoggerService();
   }
 
-  const ChildProcess = require('child_process');
-  const path = require('path');
+  /**
+   * Bootstrap the application
+   */
+  public bootstrap(): void {
+    this._logger.info(`Bootstrap application in ${this._localMode ? 'local mode' : 'production mode'}`);
 
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
+    this._logger.debug('Registering application events');
+    this.registerEvents();
 
-  const spawn = (command, a) => {
-    let spawnedProcess;
-    let error;
+    this._logger.debug('Enabling local mode if needed');
+    this.enableHotReload();
+  }
 
-    try {
-      spawnedProcess = ChildProcess.spawn(command, a, { detached: true });
-    } catch (e) {
-      error = e;
+  /**
+   * Register application events
+   */
+  private registerEvents(): void {
+    this._app.on('ready', () => this.onReady());
+    this._app.on('window-all-closed', () => this.onAllWindowClosed());
+    this._app.on('activate', () => this.onActivated());
+  }
+
+  /**
+   * Occurs when the application is ready
+   */
+  private onReady(): void {
+    this._logger.debug('Application is ready');
+    // test
+    this.initializeShell();
+  }
+
+  /**
+   * Initialize the shell
+   */
+  private initializeShell(): void {
+
+    if (this._shell != null) {
+      this._logger.debug('Shell already exist, passing initialization');
+      return;
     }
 
-    return spawnedProcess;
-  };
+    this._logger.debug('Initialize shell');
 
-  const spawnUpdate = (a: any) => {
-    return spawn(updateDotExe, a);
-  };
+    const shell = this.createShell();
+    const homePage = this.getHomePageUrl();
 
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
+    this._logger.debug(`Navigating to home page ${homePage}`);
+    shell.loadURL(homePage);
 
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
+    if (this._localMode) {
+      shell.webContents.openDevTools();
+    }
   }
 
-  return false;
-}
+  /**
+   * Create application shell
+   */
+  private createShell(): BrowserWindow {
+    const electronScreen = screen;
+    const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
-function createWindow(): BrowserWindow {
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+    // Create the browser window.
+    const shell = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width: size.width,
+      height: size.height,
+      webPreferences: {
+        nodeIntegration: true,
+        allowRunningInsecureContent: (this._localMode) ? true : false,
+      },
+    });
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-      allowRunningInsecureContent: (serve) ? true : false,
-    },
-  });
+    shell.on('closed', () => this.onShellClosed());
 
-  if (serve) {
+    return shell;
+  }
+
+  /**
+   * Occurs when the shell is closed
+   */
+  private onShellClosed(): void {
+    this._logger.debug('Shell is closed, deallocating...');
+    if (this._shell != null) {
+      this._shell = null;
+    }
+  }
+
+  /**
+   * Occurs when the app is activate
+   */
+  private onActivated(): void {
+    this._logger.debug('Applicaiton is activated');
+    this.initializeShell();
+  }
+
+  /**
+   * Occurs when all window are closed
+   */
+  private onAllWindowClosed(): void {
+    this._logger.debug('All window are closed');
+
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  }
+
+  /**
+   * Enable hot reload if needed
+   */
+  private enableHotReload(): void {
+    if (!this._localMode) {
+      this._logger.debug('Application is in local mode, hot reload not enable');
+      return;
+    }
+
+    this._logger.debug('Application is in local mode, enabling hot reload');
     require('electron-reload')(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`),
       argv: ['--serve']
     });
-    win.loadURL('http://localhost:4200');
-  } else {
-    win.loadURL(url.format({
+  }
+
+  /**
+   * Gets the home page url
+   */
+  private getHomePageUrl(): string {
+    if (this._localMode) {
+      return 'http://localhost:4200';
+    }
+
+    return url.format({
       pathname: path.join(__dirname, 'dist/portal/index.html'),
       protocol: 'file:',
       slashes: true
-    }));
+    });
   }
-
-  if (serve) {
-    win.webContents.openDevTools();
-  }
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-
-  return win;
 }
 
 try {
+  const args = process.argv.slice(1);
+  const serve = args.some(val => val === '--serve');
 
-  // this should be placed at top of main.js to handle setup events quickly
-  if (handleSquirrelEvent()) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  } else {
+  const application = new Application(serve);
+  application.bootstrap();
 
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
-    app.on('ready', createWindow);
-
-    // Quit when all windows are closed.
-    app.on('window-all-closed', () => {
-      // On OS X it is common for applications and their menu bar
-      // to stay active until the user quits explicitly with Cmd + Q
-      if (process.platform !== 'darwin') {
-        app.quit();
-      }
-    });
-
-    app.on('activate', () => {
-      // On OS X it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (win === null) {
-        createWindow();
-      }
-    });
-  }
-
-
-
-} catch (e) {
-  // Catch Error
-  // throw e;
+} catch (error) {
+  console.error(error);
 }
---
+++ projects/server/services/logger.service.ts
@@ -0,0 +1,9 @@
+export class ConsoleLoggerService {
+  public info(message: string) {
+    console.log(message);
+  }
+
+  public debug(message: string) {
+    console.log(message);
+  }
+}
